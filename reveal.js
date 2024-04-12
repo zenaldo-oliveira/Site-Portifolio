af9cff1490149b7d4cec03b5bacdf934cf6739af
zenaldo-oliveira <zenaldo_18@hotmail.com>
2024-04-11 22:33:01 -0400

    atualização biblioteca no javascript

---
+++ reveal.js
@@ -0,0 +1,124 @@
+window.revelar = ScrollReveal({reset:true})
+
+//TOPO DO SITE 
+
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+
+revelar.reveal('.efeito-img-topo',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500, 
+})
+
+// TITULOS DAS SESSÕES 
+
+revelar.reveal('.titulo',
+{
+    duration: 2000,
+    distance: '90px'
+    
+})
+
+//SESSÃO DE ESPECIALIDADES
+
+revelar.reveal('.efeito-especialidade1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay:600
+    
+})
+
+revelar.reveal('.efeito-especialidade2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay:1000
+    
+})
+
+revelar.reveal('.efeito-especialidade3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay:1500
+    
+})
+// SESSÃO SOBRE 
+
+revelar.reveal('.efeito-img-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay:500,
+    origin: 'left'
+    
+})
+
+revelar.reveal('.efeito-txt-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'right'
+
+ 
+})
+
+revelar.reveal('.efeito-txt-sobre',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'right'
+
+ 
+})
+
+revelar.reveal('.efeito-img-porta1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    
+
+ 
+})
+
+revelar.reveal('.efeito-img-porta2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    
+
+ 
+})
+
+revelar.reveal('.efeito-img-porta3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+
+ 
+})
+
+ScrollReveal().reveal('.input-custom, .textarea-custom, .submit-btn', {
+    delay: 300,
+    distance: '20px',
+    origin: 'bottom',
+    duration: 800,
+    origin: 'right'
+
+  })
+
+
+
+
